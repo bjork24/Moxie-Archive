3b3ec6e64285c5907b882f3abf8f08c301397455
Dan Chilton <dan.chilton@gmail.com>
2012-01-17 16:30:17 -0600

    Initial Commit - no changes beyond new rails instance

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moxie::Application.config.secret_token = '18d5384b59b13851d42a56a49109e1cafff7849d4ef8dac7238c289142d767912e3ad646400e5f9325285720efd68d5432db65479c5d92599b2fd8bd6df230f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Moxie::Application.config.session_store :cookie_store, :key => '_moxie_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Moxie::Application.config.session_store :active_record_store
