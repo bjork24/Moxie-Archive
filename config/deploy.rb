8585a57bc39a8e63ffb35ad89d5403a5ebbb52c2
Dan Chilton <dan.chilton@gmail.com>
2012-01-17 16:52:21 -0600

    Made some deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repository,  "."
 set :deploy_via, :copy
 
 set :user, :deploy
-set :deploy_to, "/var/www/moxie_blog"
+set :deploy_to, "/var/www/moxie"
 set :use_sudo, false
 
 role :web, application
